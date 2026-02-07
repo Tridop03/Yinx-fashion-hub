a098f2074793c2024c3184a396531f43a7a73646
Torres0407 <okuborchinonye@gmail.com>
2026-02-07 23:11:57 +0100

    assests change2

--- assets/index.ts
+++ assets/index.ts
@@ -1,21 +1,33 @@
 // Brand Assets
-export const LOGO = '/assets/yinx logo 1.png';
-export const LOGO_FOOTER = '/assets/gold  2 f 2.png';
-export const FAVICON = '/assets/image 1.png';
+import logo from './yinx logo 1.png';
+import logoFooter from './gold  2 f 2.png';
+import favicon from './image 1.png';
 
 // Product Images
+import productImg1 from './prdtImg1.png';
+import productImg2 from './prdtImg2.png';
+import productImg3 from './prdtImg3.png';
+import productImg4 from './prdtImg4.png';
+import productImg5 from './prdtImg5.png';
+import productImg7 from './prdtImg7.png';
+import productImg8 from './prdtImg8.png';
+import pinkAndBlackAnkara from './pink and black ankara.png';
+
+export const LOGO = logo;
+export const LOGO_FOOTER = logoFooter;
+export const FAVICON = favicon;
+
 export const PRODUCTS = {
-  PRODUCT_1: '/assets/prdtImg1.png',
-  PRODUCT_2: '/assets/prdtImg2.png',
-  PRODUCT_3: '/assets/prdtImg3.png',
-  PRODUCT_4: '/assets/prdtImg4.png',
-  PRODUCT_5: '/assets/prdtImg5.png',
-  PRODUCT_7: '/assets/prdtImg7.png',
-  PRODUCT_8: '/assets/prdtImg8.png',
-  PINK_AND_BLACK_ANKARA: '/assets/pink and black ankara.png'
+  PRODUCT_1: productImg1,
+  PRODUCT_2: productImg2,
+  PRODUCT_3: productImg3,
+  PRODUCT_4: productImg4,
+  PRODUCT_5: productImg5,
+  PRODUCT_7: productImg7,
+  PRODUCT_8: productImg8,
+  PINK_AND_BLACK_ANKARA: pinkAndBlackAnkara
 };
 
-// Hero and Hero Section Images
 export const HERO = {
-  PRIMARY: '/assets/pink and black ankara.png'
+  PRIMARY: pinkAndBlackAnkara
 };
